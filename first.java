2ff8126ff2390e555423a3bc7053f531aabedbe8
Sameer kadyan <gulshansameer51@gmail.com>
2025-06-28 23:38:00 +0530

    variable code

---
+++ first.java
@@ -0,0 +1,24 @@
+public class first {
+
+    // Normal Varible
+    public static void main (String[] args){
+        int a = 20;
+        int b = 34;
+        int sum = a+b;
+        System.out.println(sum);
+
+        // this output is 0
+
+        int p = 10;
+        int q = 5;
+        int ans1 = p*q/p-q;
+        System.out.println(ans1);
+
+        // this output is 10 
+
+        int x = 10;
+        int y = 5;
+        int ans2 = (x*y)/(x-y);
+        System.out.println(ans2);
+    }
+}
